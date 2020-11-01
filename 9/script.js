552ac5012dd306a4d47c1182ce7b666bab4ba603
Eddin Amaya <badreddin.amaya@skola.botkyrka.se>
2020-11-01 16:23:21 +0100

    .\9\script.js

--- 9/script.js
+++ 9/script.js
@@ -2,7 +2,8 @@ function isDivisibleByThree(num) {
     return num % 3 === 0
 }
 
-function divisiblrByThree(numbers) {
+function divisibleByThree(numbers) {
     return numbers.filter(isDivisibleByThree)
 }
 
+console.log(divisibleByThree([4, 7, 9, 3,8]))
